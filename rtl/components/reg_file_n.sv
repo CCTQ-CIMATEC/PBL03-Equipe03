3e8f1b9d0fabbf151c6c1cb30d63b8fa56b87e42
Batista-junior <deckardreplicant32@gmail.com>
2026-03-17 09:45:39 -0300

    feat: fowarding handler

--- rtl/components/reg_file_n.sv
+++ rtl/components/reg_file_n.sv
@@ -11,10 +11,15 @@ module reg_file_n(
     output logic [31:0] rd2
 );
     logic [31:0] rf [31:0];
+    
+    initial begin
+        for (int i = 0; i < 32; i++) rf[i] = 32'b0;
+    end
 
     always_ff @(negedge clk) begin
         if(we3 && (a3 != 5'b0))
             rf[a3] <= wd3;
+            $display("DEBUG RF: Escrita em x%d = %h no tempo %t", a3, wd3, $time);
     end
 
     assign rd1 = (a1 != 5'b0) ? rf[a1] : 32'b0;
